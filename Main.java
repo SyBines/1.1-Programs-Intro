6442c08e119c5b1f06040c0a129b644173789d7b
SyBines <seymourb5@nycstudents.net>
2024-09-16 23:40:40 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hi. How's it going");
+        System.out.print("Hello ");
+        String name = "Alice";
+        System.out.println("Welcome, " + name);
+
+    }
+}
